634019842219d2525259cc288ea0d93eb949dc48
Aymen-Mohammednur <aymenmohammednur12@gmail.com>
2023-06-01 14:30:47 +0300

    initial folder structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='afta_lib_python',
+    version='1.0.0',
+    author='SE The Force',
+    description='The AFTA library made for Python.',
+    packages=['afta_lib_python'],
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
